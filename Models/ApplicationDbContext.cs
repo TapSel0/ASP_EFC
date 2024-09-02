623b140aa63b57348080f07bbbe8e5cd209e46b4
Admin <Admin@DESKTOP-8A0EGBN>
2024-09-02 17:30:30 +0700

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASP_EFC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    
+        public DbSet<Customer> Customers { get; set; }
+    
+    }
+}
